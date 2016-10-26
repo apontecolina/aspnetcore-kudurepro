d14ecc291d3b6dac9a78cf1e9b2841eefd49a2a6
Tim Gabrhel <timgabrhel@gmail.com>
2016-10-26 15:04:19 -0500

    trying to break it

--- ClassLibrary2/Class1.cs
+++ ClassLibrary2/Class1.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace ClassLibrary2
 {
     public class Class1
     {
-
+        public Class1(IConfiguration configuration)
+        {
+            var test = configuration["Test"];
+            var whoops = test;
+        }
     }
 }
