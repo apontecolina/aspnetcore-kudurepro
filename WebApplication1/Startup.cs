4504798924a6fdc669135755bd02bcc7096687e9
apontecolina <apontecolina@gmail.com>
2017-06-03 17:26:26 -0400

    Fixing Startup

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -16,6 +16,7 @@ namespace WebApplication1
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +28,13 @@ namespace WebApplication1
             {
                 app.UseDeveloperExceptionPage();
             }
+
+   //         app.UseMvc(routes =>
+			//{
+			//	routes.MapRoute(
+			//		name: "default",
+			//		template: "{controller=Home}/{action=Index}/{id?}");
+			//});
 
             app.Run(async (context) =>
             {
